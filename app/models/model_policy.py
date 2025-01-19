c9cc8e8ccf1421258639f35cc0365ebdc8271ae4
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-19 09:52:08 +0100

    IATW adds ModelPolicy to define policy schema and attributes

---
+++ app/models/model_policy.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app.app import db
+from enum import Enum
+
+
+class PolicyStatus(Enum):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+
+
+class ModelPolicy(db.Model):
+    __tablename__ = "policy"
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Enum(PolicyStatus), nullable=False,
+                       default=PolicyStatus.INACTIVE.value)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now, onupdate=datetime.now)
+    created_by = db.Column(db.String(50), nullable=False)
+    length = db.Column(db.Number, nullable=False, default=12)
+    upper_case_length = db.Column(db.Number, nullable=False, default=1)
+    numbers_length = db.Column(db.Number, nullable=False, default=1)
+    special_char_length = db.Column(db.Number, nullable=False, default=1)
