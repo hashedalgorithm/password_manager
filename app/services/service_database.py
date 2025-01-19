1e5d490324957ecb1b030cf9f7bb8f34af4b3886
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-19 11:30:40 +0100

    IATW adds admin user by default

--- app/services/service_database.py
+++ app/services/service_database.py
@@ -1,8 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from app.models.model_user import ModelUser
+from datetime import datetime
 
 
 def init_tables(db: SQLAlchemy):
+
+    inti_users(db)
+
+    db.session.commit()
+
+
+def inti_users(db):
     db.session.execute(text('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,5 +20,11 @@ def init_tables(db: SQLAlchemy):
             created_at TEXT NOT NULL
         )
     '''))
+    admin = ModelUser(
+        email='system@admin.com',
+        name='admin',
+        created_at=datetime.now().isoformat()
+    )
 
-    db.session.commit()
+    db.session.add(admin)
+
