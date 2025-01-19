1cfc1e24c6581a695b000e531006537b8a21b23a
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-19 12:08:53 +0100

    IATW fixes module exports of routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
 from app.routes.route_users import blueprint_users
+from app.routes.route_policies import blueprint_policies
 
-__all__ = ['blueprint_user']
+__all__ = ['blueprint_user', 'blueprint_policies']
