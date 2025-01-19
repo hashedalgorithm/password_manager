f68d93d05edc89eaa9ffd4299827c6133b778e3d
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-19 11:30:58 +0100

    IATW adds default policy

--- app/services/service_database.py
+++ app/services/service_database.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from app.models.model_policy import ModelPolicy
 from app.models.model_user import ModelUser
 from datetime import datetime
 
@@ -7,6 +8,7 @@ from datetime import datetime
 def init_tables(db: SQLAlchemy):
 
     inti_users(db)
+    init_policies(db)
 
     db.session.commit()
 
@@ -28,3 +30,28 @@ def inti_users(db):
 
     db.session.add(admin)
 
+
+def init_policies(db):
+    db.session.execute(text('''
+        CREATE TABLE IF NOT EXISTS policies (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            created_at TEXT NOT NULL,
+            updated_at TEXT NOT NULL,
+            created_by TEXT NOT NULL,
+            length INTEGER NOT NULL,
+            upper_case_length INTEGER NOT NULL,
+            numbers_length INTEGER NOT NULL,
+            special_char_length INTEGER NOT NULL,
+        )
+    '''))
+    default_policy = ModelPolicy(
+        length=12,
+        upper_case_length=2,
+        numbers_length=2,
+        special_char_length=2,
+        created_by='system',
+        created_at=datetime.now().isoformat(),
+        updated_at=datetime.now().isoformat()
+    )
+
+    db.session.add(default_policy)
