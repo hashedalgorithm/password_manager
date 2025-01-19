db3bea26748e11d10b43eddcbc607a03bb3c51b5
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-19 12:07:26 +0100

    IATW udpates module xports of models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,6 @@
-from app.models.model_user import ModelUser
+from app.models.model_user import ModelUser, UserRole
+from app.models.model_policy import ModelPolicy, PolicyStatus
+from app.models.model_password import ModelPassword
+
+__all__ = ['ModelUser', 'UserRole', "ModelPolicy",
+           "PolicyStatus", "ModelPassword"]
